9e641943604763c8f8026b7cc7006611e8ed20cb
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-01 16:31:59 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-    if condition
+    if board[index] != " " || board[index] != ""
+        true
+    else
+        false
         
     end
 end
